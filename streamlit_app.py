b202032ad4b3d6a0e1814951328b912aa4c4d05e
youssefid15 <idrissiyoussef115@gmail.com>
2025-05-19 22:47:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -164,15 +164,5 @@ if uploaded_image:
     caption = generate_caption(model, image_tensor, vocab, device)
     st.success(f"📜 Légende générée : **{caption}**")
 
-import streamlit as st
-from PIL import Image
-
-st.title("Test image upload")
-
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-    st.write("Upload received")
-    image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded image")
 
    
